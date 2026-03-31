f1b30f6e45e220551a375a3692b5ca7dfa727423
maurojuniorr <mauro.jr@icloud.com>
2026-03-30 22:20:45 -0400

    feat: ALSA Ultimate Edition com Smart MIDI e Sync Anti-Drift

--- kmod-total/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_Module.symvers :=  /usr/src/kernels/6.19.9-100.fc42.x86_64/scripts/mod/modpost -M          -o Module.symvers -n -T modules.order -i /usr/src/kernels/6.19.9-100.fc42.x86_64/Module.symvers -e 
--- kmod-total/.snd-ns6.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_snd-ns6.mod := printf '%s\n'   snd-ns6.o | awk '!x[$$0]++ { print("./"$$0) }' > snd-ns6.mod
